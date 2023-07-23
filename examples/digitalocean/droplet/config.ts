b8c771b0be47d488fd0f05c47a93968b54f38863
Olivier Pichon <op@dzango.com>
2023-07-23 23:06:06 +0800

    feat(digitalocean/droplet): add Droplet component resource

--- examples/digitalocean/droplet/config.ts
+++ examples/digitalocean/droplet/config.ts
@@ -1,23 +1,23 @@
-import { Config, StackReferenceOutputDetails } from "@pulumi/pulumi";
+import { Config } from "@pulumi/pulumi";
 
 export const getConfig = async () => {
-    const stackConfig = new Config();
+  const stackConfig = new Config();
 
-    return {
-        image: stackConfig.require("image"),
-        name: stackConfig.require("name"),
-        projectId: stackConfig.get("projectId"),
-        region: stackConfig.require("region"),
-        size: stackConfig.require("size"),
-        sshKeyNames: stackConfig.requireObject<string[]>("sshKeyNames"),
-        userDataTemplate: stackConfig.require("userDataTemplate"),
-        users: [
-            {
-                username: "dzangolab",
-                groups: "sudo, docker",
-                publicKeys: stackConfig.requireObject<string[]>("publicKeys"),
-            },
-        ],
-        vpcUuid: stackConfig.get("vpcUuid"),
-    };
+  return {
+    image: stackConfig.require("image"),
+    name: stackConfig.require("name"),
+    projectId: stackConfig.get("projectId"),
+    region: stackConfig.require("region"),
+    size: stackConfig.require("size"),
+    sshKeyNames: stackConfig.requireObject<string[]>("sshKeyNames"),
+    userDataTemplate: stackConfig.require("userDataTemplate"),
+    users: [
+      {
+        username: "dzangolab",
+        groups: "sudo, docker",
+        publicKeys: stackConfig.requireObject<string[]>("publicKeys"),
+      },
+    ],
+    vpcUuid: stackConfig.get("vpcUuid"),
+  };
 };
