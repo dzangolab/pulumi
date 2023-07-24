097a7d418248ee8f08344ee63ed4a9de8156c2ea
Olivier Pichon <op@dzango.com>
2023-07-25 07:55:08 +0800

    chore(aws/AppUser): export AppUser

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,10 +1,12 @@
 import { AppSecret } from "./appSecret";
+import { AppUser } from "./appUser";
 import { ECRRepository } from "./ecrRepository";
 import { GitlabRunnerAWSIAMUser } from "./gitlabRunnerAwsIamUser";
 import { S3Bucket } from "./s3Bucket";
 
 export const aws = {
   AppSecret,
+  AppUser,
   ECRRepository,
   GitlabRunnerAWSIAMUser,
   S3Bucket,
