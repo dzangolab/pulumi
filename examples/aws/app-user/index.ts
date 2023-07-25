1da51d17e2cf9ca8398787cf13005f5c0d98cabf
Olivier Pichon <op@dzango.com>
2023-07-25 11:19:02 +0800

    feat: update examples

--- examples/aws/app-user/index.ts
+++ examples/aws/app-user/index.ts
@@ -1,13 +1,12 @@
+import { aws } from "@dzangolab/pulumi";
 import { interpolate } from "@pulumi/pulumi";
 
 import { getConfig } from "./config";
-/* eslint-disable-next-line node/no-unpublished-import */
-import { AppUser } from "../../../src/aws/appUser";
 
 export = async () => {
   const config = await getConfig();
 
-  const user = new AppUser(config.name, {
+  const user = new aws.AppUser(config.name, {
     accessKey: config.accessKey,
     group: config.group,
     policies: config.policies,
