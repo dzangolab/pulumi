6a04a5566df03a131efd96bf7bfc5e3e8aaeb154
Olivier Pichon <203745+opichon@users.noreply.github.com>
2023-07-29 18:22:44 +0800

    Digitalocean/app resources (#53)
    * chore: release v0.9.1
    
    * feat(digitalocean/AppResources): add DigitalOcean AppResources component resource

---
+++ src/digitalocean/appResources.ts
@@ -0,0 +1,101 @@
+import {
+  Project,
+  ProjectResources,
+  ReservedIp,
+  Volume,
+  Vpc,
+} from "@pulumi/digitalocean";
+import {
+  ComponentResource,
+  ComponentResourceOptions,
+  Output,
+} from "@pulumi/pulumi";
+
+export interface AppResourcesArguments {
+  description?: string;
+  environment?: string;
+  region: string;
+  volumeSize: number;
+}
+
+export class AppResources extends ComponentResource {
+  projectId: Output<string>;
+  reservedIpId: Output<string>;
+  volumeId: Output<string>;
+  volumeName: Output<string>;
+  vpcId: Output<string>;
+
+  constructor(
+    name: string,
+    args: AppResourcesArguments,
+    opts?: ComponentResourceOptions,
+  ) {
+    super("dzangolab:pulumi:AppResources", name, args, opts);
+
+    const project = new Project(
+      name,
+      {
+        description: args.description || `${name} infrastructure`,
+        environment: args.environment || name,
+        name,
+        purpose: "Web Application",
+      },
+      {
+        ...opts,
+        parent: this,
+      },
+    );
+
+    const vpc = new Vpc(
+      name,
+      {
+        region: args.region,
+      },
+      {
+        parent: project,
+      },
+    );
+
+    const reservedIp = new ReservedIp(
+      name,
+      {
+        region: args.region,
+      },
+      {
+        parent: project,
+      },
+    );
+
+    const volume = new Volume(
+      name,
+      {
+        description: `Block-storage volume for ${name}`,
+        initialFilesystemType: "ext4",
+        region: args.region,
+        size: args.volumeSize,
+      },
+      {
+        parent: project,
+      },
+    );
+
+    new ProjectResources(
+      name,
+      {
+        project: project.id,
+        resources: [volume.volumeUrn],
+      },
+      {
+        parent: project,
+      },
+    );
+
+    this.projectId = project.id;
+    this.reservedIpId = reservedIp.id;
+    this.volumeId = volume.id;
+    this.volumeName = volume.name;
+    this.vpcId = vpc.id;
+
+    this.registerOutputs();
+  }
+}
